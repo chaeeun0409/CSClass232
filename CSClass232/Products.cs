db74e05e02e43261b051d373b98ed68c12403b54
chaeeun0409 <s2017@e-mirim.hs.kr>
2022-08-19 10:49:39 +0900

    #2 Indexer 인덱서

---
+++ CSClass232/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass232
+{
+    class Products
+    {
+        private List<String> items = new List<String>() { "짜장면", "짬뽕", "탕수육" };
+        public string this[int i]
+        {
+            get { return items[i]; }
+            set { 
+                items[i] = value;
+                Console.WriteLine(i + " 번째 상품을 " + value + "로 설정"); }
+        }
+    }
+}
